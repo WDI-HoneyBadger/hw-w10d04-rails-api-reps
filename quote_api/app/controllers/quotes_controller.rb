8f407188a4a7396fbdc15bb86a3992cb8a3674cf
nalghamdi <nalghamdi_@hotmail.com>
2019-01-23 19:14:44 +0300

    done

---
+++ quote_api/app/controllers/quotes_controller.rb
@@ -0,0 +1,47 @@
+class QuotesController < ApplicationController
+
+        before_action :set_quote, only: [:show, :update, :destroy]
+    
+        def index 
+            @quote = Quote.where(quote_query_params)
+            render json: @quote
+        end
+    
+        def show 
+            render json: @quote
+        end
+    
+        def create 
+            @quote = Quote.create(quote_params)
+            render json: @quote
+        end
+    
+        def update
+            @quote.update_attributes(quote_params)
+            render json: @quote
+        end
+    
+        def destroy
+            @quote.destroy
+            render json: {message: "SUCCESS!"}
+        end
+    
+        private
+    
+        def set_quote
+            @quote = Quote.find(params[:id])
+        end
+    
+        def quote_params
+            params.require(:quote).permit(
+                :content, :author, :category
+            )
+        end
+    
+        def quote_query_params
+            params.permit(
+                :content, :author, :category)
+        end
+
+    
+end
